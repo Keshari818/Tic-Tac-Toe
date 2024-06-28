81ab351dd0ba338b25492d42168cec78b03486b9
Harsh Keshari <harshkeshari1201@gmail.com>
2024-06-28 12:44:06 +0530

    second change in css

--- src/Components/Square.jsx
+++ src/Components/Square.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Square = (props) => {
   return (
     <div className="square" onClick={props.onClick}>
-      <h5>{props.value}</h5>
+      <h5 className="heading">{props.value}</h5>
       
     </div>
   );
